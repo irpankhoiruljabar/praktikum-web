f01b65af0fbb5888825c6eaf2a0df353d749af9b
irpann <77085160+irpankhoiruljabar@users.noreply.github.com>
2022-05-14 23:41:14 +0700

    Delete pertemuan-6 directory

--- pertemuan-6/models/Dosen_model.php
+++
@@ -1,7 +0,0 @@
-<?php
-    class Dosen_model extends CI_Model {
-        public $id, $nama, $nidn, $gender, $tmp_lahir, $tgl_lahir, $pendidikan;
-        
-  
-    }
-?>
--- pertemuan-6/views/foot.php
+++
@@ -1,6 +0,0 @@
-<footer>
-    <p>Created by <a href="http://nurulfikri.ac.id">Pusinfo NF &copy;2021</a></p>
-</footer>
-</body>
-
-</html>
